7105643d11e09c76cd5593b4c1088b674beacd64
Hao Guo <jennnnyguo@gmail.com>
2023-05-08 12:21:05 +1000

    commit tree 5e0829c3bc38e2f5374344d35c4b4cbec31089f8

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.round(Math.random() + 1)
+
+const num3 = 100
+const num4 = 96
+const mod = num3 % num4
+
+const max = Math.max(1, 2, 3, 4, 5, 20)
